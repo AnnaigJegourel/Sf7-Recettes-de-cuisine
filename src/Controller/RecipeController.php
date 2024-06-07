6f1ccca0376c9716d9883948ff27f54b1fb8a9e1
Annaig Jegourel <jegourel@dfh-ufa.org>
2024-06-07 09:12:10 +0200

    Create Recipe Controller

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\MakerBundle\Str;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RecipeController extends AbstractController
+{
+    #[Route('/recette', name: 'recipe.index')]
+    public function index(Request $request): Response
+    {
+
+        return new Response('Recettes');
+
+        // code donné par défaut
+/*         return $this->render('recipe/index.html.twig', [
+            'controller_name' => 'RecipeController',
+        ]);
+ */    }
+
+ #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
+ //public function show(Request $request): Response
+ // mettre les paramètres au niveau de la méthdoe
+ public function show(Request $request, string $slug, int $id)
+ {
+    // Retour Json avec AbstractController
+    return $this->json(['slug' => $slug]);
+    
+    // Retour Json sans AbstractController
+   // return new JsonResponse(['slug' => $slug]);
+
+    // Retour classique
+    //return new Response('Recette ' . $slug);
+
+    // Récupérer les attributs de la requête
+    //dd($request->attributes->get('slug'), $request->attributes->get('id'));
+    // si les paramètres sont dans les arguments de la méthdoes :
+    //dd($slug, $id);
+    //dd($request);
+
+    }
+
+}
