fd2c155c8dc5fd95b048d8ea112b937268b0ad6a
Annaig Jegourel <jegourel@dfh-ufa.org>
2024-06-11 14:41:27 +0200

    Add submit button

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Recipe;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,16 +13,16 @@ class RecipeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+        // on peut préciser le type de champ, modifier le label...
             ->add('title')
             ->add('slug')
             ->add('content')
-            ->add('createdAt', null, [
-                'widget' => 'single_text',
-            ])
-            ->add('updatedAt', null, [
-                'widget' => 'single_text',
-            ])
             ->add('duration')
+            // Création du bouton submit
+            //on peut préciser le type de champ, modifier le label par défaut
+            ->add('save', SubmitType::class, [
+                'label' => "Envoyer"
+            ])
         ;
     }
 
