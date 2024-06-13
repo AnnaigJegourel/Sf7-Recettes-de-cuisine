fa5a71ab5ca38cd4ff85b7ad6f7222dc9a3f223c
Annaig Jegourel <jegourel@dfh-ufa.org>
2024-06-13 09:47:05 +0200

    Ajouter des événements pour gérer slug et dates

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -3,9 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Recipe;
+use DateTimeImmutable;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Event\PostSubmitEvent;
 use Symfony\Component\Form\Event\PreSubmitEvent;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -18,7 +21,9 @@ class RecipeType extends AbstractType
         $builder
         // on peut préciser le type de champ, modifier le label...
             ->add('title')
-            ->add('slug')
+            ->add('slug', TextType::class, [
+                'required' => false
+            ])
             ->add('content')
             ->add('duration')
             // Création du bouton submit
@@ -27,16 +32,33 @@ class RecipeType extends AbstractType
                 'label' => "Envoyer"
             ])
             ->addEventListener(FormEvents::PRE_SUBMIT, $this->autoSlug(...))
+            ->addEventListener(FormEvents::POST_SUBMIT, $this->attachTimestamps(...))
         ;
     }
 
     public function autoSlug(PreSubmitEvent $event): void
     {
+        //les données sont un tableau
         $data = $event->getData();
         if(empty($data['slug'])) {
             $slugger = new AsciiSlugger();
             $data['slug'] = strtolower($slugger->slug($data['title']));
             $event->setData($data);
+        }
+    }
+
+    public function attachTimestamps(PostSubmitEvent $event): void
+    {
+        //les données sont un objet Recipe
+        $data = $event->getData();
+        if(!($data instanceof Recipe)){
+            return;
+        }
+
+        $data->setUpdatedAt(new DateTimeImmutable());
+        //si nouvelle entité
+        if(!($data->getId())) {
+            $data->setCreatedAt(new DateTimeImmutable());
         }
     }
 
