bc07f0645d7b48bd2c2b04b40333910494fb235d
Annaig Jegourel <jegourel@dfh-ufa.org>
2024-06-26 14:58:08 +0200

    Improve findAllWithCount() creating & using a DTO

---
+++ src/DTO/CategoryWitchCountDTO.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\DTO;
+
+class CategoryWithCountDTO
+{
+    //constructeur avec seulement ce qui nous est nécessaire
+    public function __construct(
+        //readonly car initialisées que à la construction et jamais modifiées
+        public readonly int $id,
+        public readonly string $name,
+        public readonly int $count
+    )
+    {
+        
+    }
+}
--- src/Repository/CategoryRepository.php
+++ src/Repository/CategoryRepository.php
@@ -17,10 +17,18 @@ class CategoryRepository extends ServiceEntityRepository
     }
 
 
+    /**
+     * Fetches the number of recipes for each category
+     *
+     * @return CategoryWithCountDTO
+     */
     public function findAllWithCount(): array
     {
         return $this->createQueryBuilder('c')                   //crée un QueryBuilder pour l'entité courante
-            ->select('c as category', 'COUNT(c.id) as total')   // renvoie les identifiants des catégories & compte le nombre de recettes associées à chaque catégorie
+            // renvoie les identifiants des catégories & compte le nombre de recettes associées à chaque catégorie
+            //->select('c as category', 'COUNT(c.id) as total') //sans DTO
+            //avec DTO : l'objet est construit à la volée à partir des données de la bdd
+            ->select('NEW App\DTO\CategoryWithCountDTO(c.id, c.name, COUNT(c.id))')  //mot-clé pour utiliser un DTO
             ->leftJoin('c.recipes', 'r')        //jointure externe avec la relation recipes de l'entité
             ->groupBy('c.id')       //groupe les résultats par identifiant de catégorie, nécessaire pour compter le total de recettes par catégorie
             ->getQuery()
