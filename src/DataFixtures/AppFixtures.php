453c789427b15ee95392d2d14e62723394097c94
Annaig Jegourel <jegourel@dfh-ufa.org>
2024-07-11 08:58:42 +0200

    Create first fixtures in AppFixtures.php

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,46 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private readonly UserPasswordHasherInterface $hasher
+    )
+    {
+        
+    }
+
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
 
+        //On crée un user avec un rôle admin
+        $user->setRoles(['ROLE_ADMIN'])
+            ->setEmail('admin@doe.fr')
+            ->setUsername('admin')
+            ->setVerified(true)
+            ->setPassword($this->hasher->hashPassword($user, 'admin'))
+            ->setApiToken('admin_token');
+        $manager->persist($user);
+
+        //On crée 10 User simples
+        for ($i = 1; $i <= 10; $i++) {
+            $user->setRoles([])
+                ->setEmail('user{$i}@doe.fr')
+                ->setUsername('user{$i}')
+                ->setVerified(true)
+                ->setPassword($this->hasher->hashPassword($user, '0000'))
+                ->setApiToken('user{$i}');
+            $manager->persist($user);
+        }
+
+        //On envoie le tout en bdd
         $manager->flush();
     }
 }
