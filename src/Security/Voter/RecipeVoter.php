8a57159c87fbda73bd068dce8c5be7389b77d0cb
Annaig Jégourel <a_jegourel@yahoo.fr>
2024-07-15 16:05:10 +0200

    Create AdminVoter

--- src/Security/Voter/RecipeVoter.php
+++ src/Security/Voter/RecipeVoter.php
@@ -4,7 +4,7 @@ namespace App\Security\Voter;
 
 use App\Entity\Recipe;
 use App\Entity\User;
-use Symfony\Bundle\SecurityBundle\Security;
+// use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
@@ -17,10 +17,10 @@ class RecipeVoter extends Voter
     public const LIST = 'RECIPE_LIST';
     public const LIST_ALL = 'RECIPE_ALL';
 
-    public function __construct(private readonly Security $security)
-    {
+    // public function __construct(private readonly Security $security)
+    // {
         
-    }
+    // }
 
     /**
      * dit si on supporte ou non cette permission
@@ -70,9 +70,9 @@ class RecipeVoter extends Voter
                 return $subject->getAuthor()->getId() === $user->getId();
                 break;
 
-            case self::LIST_ALL:
-                return $this->security->isGranted('ROLE_ADMIN');
-                break;
+            // case self::LIST_ALL:
+            //     return $this->security->isGranted('ROLE_ADMIN');
+            //     break;
             
             case self::VIEW:
             case self::LIST:
