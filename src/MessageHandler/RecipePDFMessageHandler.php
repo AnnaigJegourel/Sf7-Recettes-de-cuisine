3061bc2dc32721ed3528242d5406acc60296384c
Annaig Jegourel <jegourel@dfh-ufa.org>
2024-07-18 17:04:36 +0200

    Add message sending

--- src/MessageHandler/RecipePDFMessageHandler.php
+++ src/MessageHandler/RecipePDFMessageHandler.php
@@ -2,15 +2,16 @@
 
 namespace App\MessageHandler;
 
+use Error;
 use App\Message\RecipePDFMessage;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 #[AsMessageHandler]
 final class RecipePDFMessageHandler
 {
     public function __construct(
-        #[Autowire('%kernel.projetc_dir%/public/pdfs')]
+        #[Autowire('%kernel.project_dir%/public/pdfs')]
         private readonly string $path
     )
     {
@@ -20,6 +21,8 @@ final class RecipePDFMessageHandler
 
     public function __invoke(RecipePDFMessage $message)
     {
+        throw new Error('errreeuuurrr!!!');
+
         //chemin d'enregistrement du pdf + contenu vide
         file_put_contents($this->path . '/' . $message->id . '.pdf', '');
 
